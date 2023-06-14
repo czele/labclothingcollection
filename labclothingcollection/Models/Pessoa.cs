5722b5450da35ce9ff0a6a60a4630b775185b98c
Camila Zelenovsky <camila_zele@hotmail.com>
2023-06-14 12:06:10 -0300

    model pessoa

--- labclothingcollection/Models/Pessoa.cs
+++ labclothingcollection/Models/Pessoa.cs
@@ -1,6 +1,17 @@
-﻿namespace labclothingcollection.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace labclothingcollection.Models
 {
     public class Pessoa
     {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Genero { get; set; }
+
+        [Required(ErrorMessage = "Campo data de nascimento é obrigatório")]
+        [DisplayFormat(DataFormatString = "mm/dd/yyyy")]
+        public string DataNascimento { get; set; }
+        public string Cpf { get; set; }
+        public string Telefone { get; set; }
     }
 }
