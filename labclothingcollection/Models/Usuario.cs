1018ba1b9b4120bbffed1669690f5aca8a1a1430
Camila Zelenovsky <camila_zele@hotmail.com>
2023-06-14 12:08:53 -0300

    model Usuario

---
+++ labclothingcollection/Models/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace labclothingcollection.Models
+{
+    public class Usuario : Pessoa
+    {
+        [Required(ErrorMessage = "Campo e-mail de nascimento é obrigatório")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "E-mail em formato inválido")]
+        public string email { get; set; }
+        public string tipo { get; set; }
+        public string status { get; set; }
+
+    }
+}
