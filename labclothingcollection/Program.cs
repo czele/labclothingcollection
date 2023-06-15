3ce45bfb3267e7e35ab4cd3a5d7495a8a3a1b04f
Camila Zelenovsky <camila_zele@hotmail.com>
2023-06-14 22:47:06 -0300

    Adicionando a conexão do banco de dados

--- labclothingcollection/Program.cs
+++ labclothingcollection/Program.cs
@@ -1,3 +1,6 @@
+using labclothingcollection.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-//builder.Services.AddDbContext<>
+
+builder.Services.AddDbContext<LabClothingCollectionContext>(options => options.UseSqlServer(builder.Configuration.
+                                                                            GetConnectionString("ServerConnection")));
+
+builder.Services.AddControllers().AddNewtonsoftJson(
+    x => x.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
 
 var app = builder.Build();
 
